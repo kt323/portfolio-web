05a3989ddc8bfd2538e25d89bb6d810ce90f7804
kt323 <kthodesign@gmail.com>
2023-08-21 01:07:41 -0700

    Change_1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+alert('Hello world');
+let favoriteFood = 'Stawberries';
+document.write(favoriteFood);
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
+
