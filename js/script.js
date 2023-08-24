771e8ebf7dc83c4236ca3b6abeb18316a13d2bf2
kt323 <kthodesign@gmail.com>
2023-08-23 18:52:30 -0700

    pokemonList

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
-let pokemonList = [];
-pokemonList = [
-{
-    name: Charizard,
-    height: 1.7,
-    type: ['fire', 'flying'],
-    speed: 100
-},
-{
-    name: Psyduck,
-    height: 0.8,
-    type: water,
-    speed: 55
-},
-{
-    name: Dewgong,
-    height: 1.7,
-    type: ['water', 'ice'],
-    speed: 70
-}
-]
+let pokemonList = [
+    {
+        name: 'Charizard',
+        height: 1.7,
+        type: ['fire', 'flying'],
+        speed: 100
+    },
+    {
+        name: 'Psyduck',
+        height: 0.8,
+        type: ['water'],
+        speed: 55
+    },
+    {
+        name: 'Dewgong',
+        height: 1.7,
+        type: ['water', 'ice'],
+        speed: 70
+    }
+];
