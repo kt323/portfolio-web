87d270c5d39e32e5f34d8c0fe743070f536e7375
kt323 <kthodesign@gmail.com>
2023-09-11 00:43:12 -0700

    Exercise 1.6

--- js/script.js
+++ js/script.js
@@ -1,36 +1,114 @@
-let pokemonList = [];
-pokemonList = [
-    {
-        name: 'Charizard',
-        height: 1.7,
-        type: ['fire', 'flying'],
-        speed: 100 
-    },
-    {
-        name: 'Psyduck',
-        height: 0.8,
-        type: ['water'],
-        speed: 55
-    },
-    {
-        name: 'Dewgong',
-        height: 1.7,
-        type: ['water', 'ice'],
-        speed: 70
-    }
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+      {
+          name: 'Charizard',
+          height: 1.7,
+          type: ['fire', 'flying'],
+          speed: 100 
+      },
+      {
+          name: 'Psyduck',
+          height: 0.8,
+          type: ['water'],
+          speed: 55
+      },
+      {
+          name: 'Dewgong',
+          height: 1.7,
+          type: ['water', 'ice'],
+          speed: 70
+      }
+  ];
 
-const pokemonContainer = document.getElementById('pokemon-container');
+  function getAll() {
+      return pokemonList;
+  }
 
-pokemonList.forEach(pokemon => {
-    let output = pokemon.name + ' (height: ' + pokemon.height + ')';
+  function add(pokemon) {
+      if (
+          typeof pokemon === "object" &&
+          "name" in pokemon &&
+          "height" in pokemon &&
+          "type" in pokemon
+      ) {
+          pokemonList.push(pokemon);
+      } else {
+          console.log("Pokemon is not correct");
+      }
+  }
 
-    if (pokemon.height > 1.6) {
-        output += ' - Wow, that\'s big!';
-    }
+  function showDetails(pokemon) {
+      console.log(pokemon);
+  }
 
-    let pokemonInfo = document.createElement('p');
-    pokemonInfo.textContent = output;
+  function addListItem(pokemon) {
+      let pokemonListContainer = document.querySelector('.pokemon-list');
+      let listpokemon = document.createElement('li');
+      let button = document.createElement('button');
+      button.innerText = pokemon.name;
+      button.classList.add('button-class');
+      listpokemon.appendChild(button);
+      pokemonListContainer.appendChild(listpokemon);
 
-    pokemonContainer.appendChild(pokemonInfo);
+      button.addEventListener('click', function () {
+          showDetails(pokemon);
+      });
+  }
+
+  return {
+      getAll: getAll,
+      add: add,
+      addListItem: addListItem
+  };
+})();
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
 });
+
+/*console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: "Charizard", height: 1.7, types: ["fire"]});
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
+
+  var size;
+  if (item.height > 1) {
+    size = "Wow, that\'s big!";
+  } else {
+    size = "It\'s small pokemon";
+  }
+
+  var result;
+  item.types.forEach(function (typeItem) {
+    if (typeItem == "fire") {
+      result = '<span styple="color:orange;"> ';
+    } else if (typeItem == "water") {
+      result = '<span style="color:blue"> ';
+    } else if (typeItem == "flying") {
+      result = '<span style="color:green"> ';
+    } else if (typeItem == "ice") {
+      result = '<span style="color:yellow"> ';
+    }
+});
+document.write {
+  '<div class="box">' +
+    item.name +
+    "(height: " +
+    item.height +
+    "m" +
+    ")" +
+    "<br>" +
+    item.types +
+    "<br>" +
+    "</div"
+};
+});
+
+
+//function add (pokemon) {
+  //pokemonList.push(pokemon);
+//}*/
